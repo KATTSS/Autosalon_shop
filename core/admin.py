4c9424e72ab076aa5feee428f6d3c15f6e241286
KATTSS <katsyarynnks@gmail.com>
2026-05-10 13:15:18 +0300

    Feature: formatting of admin stage

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import CompanyInfo, Employee, PickupPoint
 
-admin.site.register(CompanyInfo)
-admin.site.register(Employee)
-admin.site.register(PickupPoint)
+@admin.register(CompanyInfo)
+class CompanyInfoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'logo', 'history', 'requisites')
 
+@admin.register(Employee)
+class EmplouyeeAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'position', 'phone', 'email','photo', 'description', 'user')
+
+@admin.register(PickupPoint)
+class PickupPointAdmin(admin.ModelAdmin):
+    list_display = ('address', 'working_hours', 'phone', 'description')
