9dad1383badf570a4ac89324caa43ad40c69699c
KATTSS <katsyarynnks@gmail.com>
2026-05-10 11:07:02 +0300

    Feature: add models registration in admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CompanyInfo, Employee, PickupPoint
 
-# Register your models here.
+admin.site.register(CompanyInfo)
+admin.site.register(Employee)
+admin.site.register(PickupPoint)
+
--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ProductType, Customer, Manufacturer, Product, Sale, SaleItem, Supplier, Supply
 
-# Register your models here.
+admin.site.register(ProductType)
+admin.site.register(Customer)
+admin.site.register(Manufacturer)
+admin.site.register(Product)
+admin.site.register(Sale)
+admin.site.register(SaleItem)
+admin.site.register(Supplier)
+admin.site.register(Supply)
